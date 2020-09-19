cc79045ffbd39f4c11fed4cbc430b8d2409ee6bd
Grigory Entin <grigory.entin@gmail.com>
2020-09-19 09:54:33 +0200

    Added preliminary support for Swift packages.

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "GETracing",
+    products: [
+        .library(
+            name: "GETracing",
+            targets: ["GETracing"]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "GETracing",
+            dependencies: [],
+            exclude: ["ModuleExports-GETracing.swift"],
+            swiftSettings: [
+                .define("GE_TRACE_ENABLED")
+            ]
+        ),
+        .testTarget(
+            name: "GETracingTests",
+            dependencies: ["GETracing"]
+        ),
+    ]
+)
